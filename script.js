94f7d83460002c420c1a449036f53be56e4f0810
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2023-04-14 16:39:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+myFunction() {
+     var x;
+     if (confirm("Are you sure?") == true) {
+         x = "You pressed OK!";
+     } else {
+         x = "You pressed Cancel!";
+     }
+     return x; 
+}
+myFunction();
